9d47cee2d7cc9cf72ace6e679ccb318e9cd7622f
syafiq-suhaimi <devsyafiq86@gmail.com>
2019-11-05 12:34:13 +0800

    initial commit for senarai kehadiran page

--- src/app/senarai-kehadiran/senarai-kehadiran.module.ts
+++ src/app/senarai-kehadiran/senarai-kehadiran.module.ts
@@ -5,12 +5,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
 
-import { QrcodePage } from './qrcode.page';
+import { SenaraiKehadiranPage } from './senarai-kehadiran.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: QrcodePage
+    component: SenaraiKehadiranPage
   }
 ];
 
@@ -21,6 +21,6 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [QrcodePage]
+  declarations: [SenaraiKehadiranPage]
 })
-export class QrcodePageModule {}
+export class SenaraiKehadiranPageModule {}
---
+++ src/app/senarai-kehadiran/senarai-kehadiran.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-senarai-kehadiran',
+  templateUrl: './senarai-kehadiran.page.html',
+  styleUrls: ['./senarai-kehadiran.page.scss'],
+})
+export class SenaraiKehadiranPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
